42963e2f5b0c308d62b49cabba6c850f0dc62661
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-17 12:05:33 -0700

    Doctest

---
+++ Testing/doctesting.py
@@ -0,0 +1,15 @@
+def add(x ,y):
+	"""
+	>>> add(2, 3)
+	5
+	>>> add(100,200)
+	300
+	"""
+	return x * y
+
+'''
+To execute the above file, use the following;
+
+python3 -m doctest -v doctesting.py 
+
+'''
