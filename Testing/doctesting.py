16cc61fb10f033771c8e3132f265b67ee47b313d
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-17 12:17:34 -0700

    Doctest update

--- Testing/doctesting.py
+++ Testing/doctesting.py
@@ -13,3 +13,23 @@ To execute the above file, use the following;
 python3 -m doctest -v doctesting.py 
 
 '''
+
+def double(values):
+	""" double the values in a list
+
+	>>> double([1,2,3,4])
+	[2, 4, 6, 8]
+	
+	>>> double([])
+	[]
+
+	>>> double(['a','b','c'])
+	['aa', 'bb', 'cc']
+
+	>>> double([True, None])
+	Traceback (most recent call last):
+		...
+	TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+	"""
+
+	return [2 * element for element in values]
