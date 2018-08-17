2b65b74566c8192971f503357be7fbfbce001d62
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-16 22:14:43 -0700

    global regex pattern

---
+++ Regex/books_sort.py
@@ -0,0 +1,23 @@
+import re
+
+titles = [
+    "Significant Others (1987)",
+    "Tales of the City (1978)",
+    "The Days of Anna Madrigal (2014)",
+    "Mary Ann in Autumn (2010)",
+    "Further Tales of the City (1982)",
+    "Babycakes (1984)",
+    "More Tales of the City (1980)",
+    "Sure of You (1989)",
+    "Michael Tolliver Lives (2007)"
+]
+
+test = []
+
+pattern = re.compile(r'(?P<first>^[\w ]+) \((?P<last>\d{4})\)')
+for book in titles:
+	matches = pattern.sub("\g<last> - \g<first>", book)
+	test.append(matches)
+
+test.sort()
+print(test)
