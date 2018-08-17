2efde9011954a3c94cae3dd010f65998d12a7804
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-17 00:36:55 -0700

    file copy

---
+++ FileIO/copy.py
@@ -0,0 +1,8 @@
+def copy(file_name, new_file_name):
+    with open(file_name) as file:
+        text = file.read()
+    
+    with open(new_file_name, "w") as new_file:
+        new_file.write(text)
+
+copy('Script.txt', 'Script_copy.txt')
