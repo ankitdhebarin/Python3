9ceabc6afb76f6fc18a63addfe5b0a81942d0279
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-18 01:09:57 -0700

    custom for loop

---
+++ Iterators_Generators/Counter.py
@@ -0,0 +1,18 @@
+class Counter():
+
+	def __init__(self, low, high):
+		self.low = low
+		self.high = high
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.low < self.high:
+			num = self.low
+			self.low += 1
+			return num
+		raise StopIteration
+
+for num in Counter(0,10):
+	print(num)
