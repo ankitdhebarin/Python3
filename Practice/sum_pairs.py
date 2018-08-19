b51771065bcad8ee47930cab54165a768f327cc5
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-19 01:36:56 -0700

    sum pairs

--- Practice/sum_pairs.py
+++ Practice/sum_pairs.py
@@ -2,15 +2,17 @@ def sum_pairs(mylist,n):
 
 	size = len(mylist)
 	sums = []
+	found = False
 	for i in range(size):
 		for j in range(i+1,size):
 			
+			if len(sums) != 0:
+				return sums
+
 			if mylist[i] + mylist[j] == n:
 				sums.append(mylist[i])
 				sums.append(mylist[j])
-		
-
+				
 	return sums
 
-x = sum_pairs([4,2,10,5,1], 6)
-print(x)
+print(sum_pairs([4,2,10,5,1], 6))
