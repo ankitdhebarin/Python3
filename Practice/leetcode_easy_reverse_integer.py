f1cbd3e58adcf2fb241226d253ca48d88cdd09f7
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-26 15:16:46 -0700

    reverse int

---
+++ Practice/leetcode_easy_reverse_integer.py
@@ -0,0 +1,22 @@
+def reverseint(num):
+
+	i = 0
+	temp = 0
+	s = str(num)
+
+	if len(s) == 1:
+		return int(s)
+
+	else:
+
+		while i <= len(s)-1:
+			rem = num % 10
+			temp = temp * 10 + rem
+			num = num // 10
+			i += 1
+
+	return int(temp)
+
+print(reverseint(247))
+
+
