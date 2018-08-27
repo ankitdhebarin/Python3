7257c3e70b94153b83245483b594d7397071e78f
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-08-26 20:31:03 -0700

    merge 2 sorted lists

--- Practice/leetcode_mergelists.py
+++ Practice/leetcode_mergelists.py
@@ -6,13 +6,13 @@ def mergeTwoLists(l1, l2):
 
 	while l1 and l2:
 
-		if l1[0] == l2[0]:
+		if l1[0] < l2[0]:
 			res.append(l1.pop(0))
 		else:
 			res.append(l2.pop(0))
 
 
-	return sorted(res + l1 + l2)
+	return res + l1 + l2
 
-print(mergeTwoLists([1,2], [1,3,7]))
+print(mergeTwoLists([1,2,5,8,0], [1,3,7]))
 
