b29329fb70a79ad2b6f351a427cc4d6aad51e97d
Ankit Dhebar <ankitdhebarin@gmail.com>
2018-11-11 15:05:23 -0800

    headers with json and plain text

--- HTTP_Requests/headers.py
+++ HTTP_Requests/headers.py
@@ -8,3 +8,6 @@ response2 = requests.get(url, headers={"Accept":"application/json"})
 print(response1.text)
 print(response1.json())
 print(response2.text)
+
+data = response2.json()
+print(data['joke'])
